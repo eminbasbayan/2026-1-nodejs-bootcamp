56bc172f329bc59107d31f28f138205fc2c06313
eminbasbayan <emin.basbayan@icloud.com>
2026-01-22 23:03:18 +0300

    events module

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const path = require("node:path");
+const EventEmitter = require('node:events');
+const emitter = new EventEmitter();
 
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
+emitter.on('orderPizza', (boyut, icindekiler) => {
+  console.log(`Bir ${boyut} pizza pişiriliyor. İçindekiler: ${icindekiler}`);
+});
 
-// console.log(path.extname(__filename));
-// console.log(path.extname("index.css"));
-
-// let parsedPath = path.parse(__filename);
-
-// console.log(parsedPath);
-
-// console.log(path.format(parsedPath));
-
-// console.log(path.isAbsolute("C:/Users/eminb/Desktop/2026-1-nodejs-bootcamp"));
-// console.log(path.isAbsolute("./data.json"));
-// console.log(__filename);
-
-// console.log(path.join("musteriler", "usernames", "faturalar", "fatura.pdf"));
-
-// console.log(path.resolve('folder1', 'folder2', 'index.html')); // Mutlak yol
-
-
-
-
-
-// console.log(__filename);
-// console.log(__dirname);
+emitter.emit("orderPizza", "small", "peynir");
