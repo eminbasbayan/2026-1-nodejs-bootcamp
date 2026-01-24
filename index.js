0c797aeef378a4337ce41b15ba85cf8eb0472642
eminbasbayan <emin.basbayan@icloud.com>
2026-01-24 21:50:50 +0300

    fs promise module

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-const fs = require("node:fs");
+const fs = require("node:fs").promises;
 
-// Senkron dosya okuma
-// const txtFile = fs.readFileSync("./file.txt", "utf-8");
-// console.log(txtFile.toString());
-// console.log("sync");
-
-// Asenkron dosya okuma
-/* fs.readFile("./file.txt", "utf-8", (err, data)=>{
-   if(err){
-      console.log(err);
-   }else{
-      console.log(data);
-   }
-})
-console.log("async"); */
-
-// Dosya içi yazıyı senkron değiştirme
-/* fs.writeFileSync("./file.txt", "Bu bir test dosyasıdır."); */
-
-// Dosya içi yazıyı asenkron değiştirme
-/* fs.writeFile("./file2.txt", "Emin Başbayan", (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Dosya başarıyla yazıldı!");
-
+async function readFile() {
+    try {
+        const data = await fs.readFile("./file.txt", "utf-8");
+        console.log(data);
+    } catch (error) {
+        console.log(error);
     }
-}) */
 
-// Dosyaya sonuna yeni veri ekleme
-fs.writeFile("./file.txt", "Yeni Eklenen Veri", { flag: "a" }, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Dosya başarıyla yazıldı!");
-    }
-})
+}
+
+readFile()
 
