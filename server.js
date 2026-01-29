fc5ab5b5e0d61c225f505745b579192f687c70b1
eminbasbayan <emin.basbayan@icloud.com>
2026-01-29 22:57:47 +0300

    middleware kullanımı

--- server.js
+++ server.js
@@ -11,23 +11,42 @@ const customers = [
   },
 ];
 
+const role = 'user';
+
+const isAdmin = (req, res, next) => {
+  if (req.role === 'admin') {
+    next();
+  } else {
+    res.status(401).send('Kullanıcı yetkisi yok!');
+  }
+};
+
+function isLogin(req, res, next) {
+  if (role === 'user') {
+    next();
+  } else {
+    res.status(401).send('Lütfen giriş yapınız!');
+  }
+}
+
 app.get('/', (request, response) => {
   response.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/products-page', (req, res) => {
+app.get('/products-page', isAdmin, (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'views', 'products.html'));
 });
 
-app.get('/new-page', (req, res) => {
+app.get('/new-page', isLogin, (req, res) => {
+  // db işlemleri
   res.status(200).sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
-app.get("/old-page", (req, res)=> {
-  res.redirect(301, "/new-page")
-})
+app.get('/old-page', (req, res) => {
+  res.redirect(301, '/new-page');
+});
 
-app.get('/api/customers', (req, res) => {
+app.get('/api/customers', isAdmin, (req, res) => {
   res.status(200).json(customers);
 });
 
