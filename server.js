043b67e35e07ffe040e6588be84bb6c63a2e2b36
eminbasbayan <emin.basbayan@icloud.com>
2026-01-29 22:24:39 +0300

    express routing

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
 const PORT = 3000;
 
+const customers = [
+  {
+    id: 1,
+    firstName: 'Emin',
+    lastName: 'Başbayan',
+  },
+];
+
 app.get('/', (request, response) => {
-  response.send('Hello World!');
+  response.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/products-page', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'views', 'products.html'));
+});
+
+app.get('/api/customers', (req, res) => {
+  res.status(200).json(customers);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page not found!');
 });
 
 app.listen(PORT, () => {
