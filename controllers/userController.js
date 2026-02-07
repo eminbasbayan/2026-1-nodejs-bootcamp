6d094ceab6df657d5e679c4986f1d5cb3428fa9b
eminbasbayan <emin.basbayan@icloud.com>
2026-02-07 21:20:28 +0300

    register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,9 +57,35 @@ const deleteUser = (req, res) => {
   res.status(200).json(users);
 };
 
+const registerUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const newUser = { id: Math.random(), email, password };
+
+    const usersFilePath = path.join(__dirname, '..', 'models', 'users.json');
+    const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+    const findUser = users.find((user) => user.email === email);
+
+    if (findUser) {
+      return res
+        .status(400)
+        .json({ message: 'Bu email adresi zaten kayıtlı!' });
+    }
+
+    users.push(newUser);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllUsers,
   createNewUser,
   updateUser,
   deleteUser,
+  registerUser
 };
