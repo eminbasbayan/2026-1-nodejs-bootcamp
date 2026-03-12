5df1064a410d9801b1c9234759ab781b23f44f6f
eminbasbayan <emin.basbayan@icloud.com>
2026-03-12 22:50:56 +0300

    user service

--- config/iyzico.js
+++
@@ -1,9 +0,0 @@
-const Iyzipay = require('iyzipay');
-
-const iyzipay = new Iyzipay({
-    apiKey: process.env.IYZICO_API_KEY,
-    secretKey: process.env.IYZICO_SECRET_KEY,
-    uri: process.env.IYZICO_BASE_URL || 'https://sandbox-api.iyzipay.com',
-});
-
-module.exports = iyzipay;
