1cb18daf19c695311c9c369efd4b2afc6341229c
eminbasbayan <emin.basbayan@icloud.com>
2026-04-23 21:56:20 +0300

    user get response değişti

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ const getAllUsers = async (req, res) => {
       .sort({ createdAt: -1 })
       .lean();
 
-    res.status(200).json({ success: true, count: users.length, data: users });
+    res.status(200).json({ success: true, count: users.length, data: users, fullName: "Emin Başbayan" });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
